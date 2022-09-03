32bc9c9adb75f130484d76960f01eec543cc7450
yzbaya <101043195+yzbaya@users.noreply.github.com>
2022-09-03 15:37:43 +0200

    add removeposts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useEffect, useState } from 'react';
+import Post from './post';
 
-function App() {
+//boutton illi trahja3 lel 0
+
+const App = ({initialCount}) => {
+  const [state, setState] = useState({
+    count: initialCount,
+    user:"Francis"
+  });
+
+
+ //fct mta3 les information illi 9a3dine yetzedou
+
+  let [posts,setPosts] = useState([
+    { 
+      name:'Super awesome post',
+      body:'The content of the post'
+    },
+    { 
+      name:'JS is great',
+      body:'something else'
+    },
+  ])
+
+
+//fct mta3 l boutton illi 9a3da tna9es
+  const restOne = () => {
+    setState({
+      ...state,
+      count:state.count-1
+    })
+  }
+
+//l ma3loumet illi bech yetzedou fi le5er ki nenzel 3al boutton mta3 add on more
+  const addOnePost = () => {
+    let newPost = {
+      name:'PHP is still awesome',
+      body:'something about php'
+    }
+
+    setPosts([
+      ...posts,
+      newPost
+    ]);
+
+  }
+//boutton illi tna7i les paragraphes
+const removePosts=()=>{
+setPosts([])
+}
+
+ useEffect(()=>{
+ //console.log('change on state') 
+ },[state])
+
+ useEffect(()=>{
+//console.log('change on posts') 
+ },[posts])
+
+ useEffect(()=>{
+//console.log('mounted')
+ },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{state.user}</h1>
+      <h3>Count: {state.count}</h3>
+
+
+      <button onClick={ ()=> setState({
+        //ki n7otou state ya3ni idha na9sou wala nzidou 1 "francis" tab9a mawjouda
+        ...state,count:state.count+1 }) }> Add one +1 </button>
+       
+      <button onClick={ restOne }> Rest one -1 </button>
+       <button onClick={ removePosts}> remove posts </button> 
+
+      <hr/>
+
+      { posts.map((item,i)=>(
+        <Post key={i} item={item} />
+      ))}
+
+      <button onClick={addOnePost}>
+        
+        Add on more 
+        
+      </button>
+
+    </>
   );
 }
 
 export default App;
+
+//array because you get the value and the function witch change the value
---
+++ src/post.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+
+const Post = ({item}) => {
+
+ useEffect(()=>{
+
+    console.log('post created')
+
+    return()=>{
+        //
+        console.log('component created')
+    }
+ },[])
+ 
+    return(
+        <div>
+            <div>Name: {item.name} </div>
+            <div>Body: {item.body} </div>
+            <hr/>
+        </div>  
+    )
+}
+
+
+export default Post;
